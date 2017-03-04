b99fbb968bba1270d43d13bfdb36ad408305fa70
ueda kentaro <nowallcloud9+ueda@gmail.com>
2017-03-04 14:49:17 +0000

    基本課題_15完了

--- app/assets/javascripts/timelines.js
+++ app/assets/javascripts/timelines.js
@@ -1,6 +1,5 @@
 $(function(){
   $('form.input_message_form input.post').click(function(e){
-    // 「Post」ボタンは非Ajaxにする
     var form = $('form.input_message_form');
     form.removeAttr('data-remote');
     form.removeData("remote");
@@ -8,10 +7,18 @@ $(function(){
   });
 
   $('form.input_message_form').on('ajax:complete', function(event, data, status){
-    // Ajaxレスポンス
     if ( status == 'success') {
       var json = JSON.parse(data.responseText);
+      if (json.timeline){
+      $('div.alert').empty();
       $('div.timeline').prepend(json.timeline);
+      }
+        else if (json.error){
+            $('div.alert').empty();
+            for( var i in json.error) {
+                $('div.alert').append($('<p>' + json.error[i] + '</p>'));
+            } 
+        }
     }
   });
 });
--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -14,11 +14,27 @@ class TimelinesController < ApplicationController
     timeline.attributes = input_message_param
     timeline.user_id = current_user.id
     if timeline.valid?
-      timeline.save
+      timeline.save!
+        respond_to do |format|
+          format.html do
+            redirect_to action: :index
+          end
+          format.json do
+            html = render_to_string partial:'timelines/timeline', layout: false, formats: :html, locals: {t: timeline}
+            render json:{timeline:html}
+          end
+        end
     else
-      flash[:alert] = timeline.errors.full_messages
+      respond_to do |format|
+        format.html do
+          flash[:alert] = timeline.errors.full_messages
+          redirect_to action: :index
+        end
+        format.json do
+          render json: {error: timeline.errors.full_messages}
+        end
+      end
     end
-    redirect_to action: :index
   end
   
   def update
